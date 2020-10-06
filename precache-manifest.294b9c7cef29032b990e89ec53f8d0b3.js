d0d7a1d42838366b88f250f976c05c0ae5a458ba
slightly-nerdy <github@slightlynerdy.org>
2020-10-06 12:09:48 +0100

    Updates

---
+++ precache-manifest.294b9c7cef29032b990e89ec53f8d0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f357f897d61c17d2399fd440e467f60b",
+    "url": "/react-draggable/index.html"
+  },
+  {
+    "revision": "2e785a5b9fb53526021a",
+    "url": "/react-draggable/static/css/main.f62810b7.chunk.css"
+  },
+  {
+    "revision": "a6e07a59f682cbc1f088",
+    "url": "/react-draggable/static/js/2.8930bc88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-draggable/static/js/2.8930bc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e785a5b9fb53526021a",
+    "url": "/react-draggable/static/js/main.dd38ad44.chunk.js"
+  },
+  {
+    "revision": "4b1b15ad4ac3ba241d91",
+    "url": "/react-draggable/static/js/runtime-main.b19bf064.js"
+  }
+]);
