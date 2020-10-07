c21d78e8585ee2057f6b0a7b9b683acde116d22e
slightly-nerdy <github@slightlynerdy.org>
2020-10-07 16:46:08 +0100

    Refactoring to help with drop support

--- src/board.jsx
+++ src/board.jsx
@@ -4,14 +4,14 @@ import { processSquareClick } from './game';
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-function requiredDefined(...args) {
-    args.forEach(arg => {
-        if (arg === undefined) {
-            console.log('required arguments in undefined in', args)
-            throw new Error('required arguments in undefined in', args);
-        }
-    });
-}
+// function requiredDefined(...args) {
+//     args.forEach(arg => {
+//         if (arg === undefined) {
+//             console.log('required arguments in undefined in', args)
+//             throw new Error('required arguments in undefined in', args);
+//         }
+//     });
+// }
 
 function Square({ black, children, onClick }) {
     const className = 'square' + (black ? ' blackSquare' : '');
@@ -19,26 +19,26 @@ function Square({ black, children, onClick }) {
         <div className={className} onClick={onClick}>
             {children}
         </div>
-
     );
 }
 
-
-function renderSquare(row, col, boardProps) {
+function makeBoardSquare(index, boardProps) {
     const { config, knightPosition } = boardProps;
-    const { nRows, nCols, topLeftBlack } = config;
-    requiredDefined(nRows, nCols, topLeftBlack, knightPosition);
+    const { nCols, topLeftBlack } = config;
+        
+    const col = index % nCols;
+    const row = Math.floor(index / nCols);
 
     const isKnight = (row === knightPosition[0]) && (col === knightPosition[1]);
-    const asTopleft = (row + col) % 2 === 0;
-    const black = topLeftBlack ? asTopleft : !asTopleft;
-    const index = row * nCols + col;
+
+    const sameColorTopleft = (row + col) % 2 === 0;
+    const black = sameColorTopleft ? topLeftBlack : !topLeftBlack;
+    
     return (
-        <Square black={black} onClick={() => processSquareClick(row, col)} key={[row, col]} index={index}>
+        <Square black={black} onClick={() => processSquareClick(row, col)} key={index} index={index}>
             {isKnight ? <Knight /> : null}
         </Square>
     );
-
 }
 
 function Board(props) {
@@ -46,10 +46,8 @@ function Board(props) {
     const { nRows, nCols } = config;
 
     let squares = [];
-    for (let row = 0; row < nRows; ++row) {
-        for (let col = 0; col < nCols; ++col) {
-            squares.push(renderSquare(row, col, props));
-        }
+    for (let index = 0; index < nRows * nCols; ++index) {
+        squares.push(makeBoardSquare(index, props));
     }
 
     const style = { // For now
